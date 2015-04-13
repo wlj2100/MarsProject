450e76158a7037b097c0188b5aba5a500b045831
wlj2100 <wlj2100@gmail.com>
2015-04-13 01:03:02 -0500

    modify login page for server use(do not need for project) add module clase

---
+++ MarsProject/src/mars/client/Module.java
@@ -0,0 +1,19 @@
+package mars.client;
+
+public class Module {
+	// module properties
+    int code;
+    String status;
+    int turns;
+    int xcoord;
+    int ycoord;
+    // constructor
+    public Module(int code, String status, int turns, int xcoord, int ycoord) {
+    	super();
+    	this.code=code;
+    	this.status=status;
+    	this.turns=turns;
+    	this.xcoord=xcoord;
+    	this.ycoord=xcoord;
+    }
+}
