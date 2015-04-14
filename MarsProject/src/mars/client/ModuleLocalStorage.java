41f269038aaab17cf83e92715a5c6d86fe4656d6
wlj2100 <wlj2100@gmail.com>
2015-04-13 21:49:07 -0500

    a little modify for moduleLocalStorage

--- MarsProject/src/mars/client/ModuleLocalStorage.java
+++ MarsProject/src/mars/client/ModuleLocalStorage.java
@@ -9,9 +9,13 @@ import com.google.gwt.storage.client.Storage;
  */
 public class ModuleLocalStorage {
 	Storage moduleStore;
+	
+	//constructor
 	public ModuleLocalStorage() {
 		moduleStore=Storage.getLocalStorageIfSupported();
 	}
+	
+	//key = type+number
 	public void addModule(String type, Module module) {
 		moduleStore.setItem(type+Integer.toString(moduleStore.getLength()+1), module.toString());
 	}
