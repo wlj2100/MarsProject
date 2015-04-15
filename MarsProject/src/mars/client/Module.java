945db15c196b5489bcbbae6f7d899fbe4d171b4a
frees033 <frees033@d.umn.edu>
2015-04-15 18:59:39 -0500

    Module Additions

--- MarsProject/src/mars/client/Module.java
+++ MarsProject/src/mars/client/Module.java
@@ -3,7 +3,6 @@ package mars.client;
 import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
-import com.google.gwt.json.client.JSONString;
 
 /**
  * @author Liangji
@@ -13,16 +12,18 @@ public class Module {
 	
 	// module properties
     int code;
-    String status;
+    int status;
     int turns;
     int xcoord;
     int ycoord;
+	String theString;
+	public int getCode() {return this.code;}
     
     // constructor
     public Module() {
     	super();
     }
-    public Module(int code, String status, int turns, int xcoord, int ycoord) {
+    public Module(int code, int status, int turns, int xcoord, int ycoord) {
     	super();
     	this.code=code;
     	this.status=status;
@@ -30,15 +31,15 @@ public class Module {
     	this.xcoord=xcoord;
     	this.ycoord=xcoord;
     }
+	
     public Module(String moduleString) {
     	super();
     	JSONObject jO=(JSONObject)JSONParser.parseLenient(moduleString);
     	JSONNumber jN;
-    	JSONString jS;
     	jN = (JSONNumber)jO.get("code");
     	this.code = (int)jN.doubleValue();
-    	jS = (JSONString)jO.get("status");
-    	this.status = jS.stringValue();
+    	jN = (JSONNumber)jO.get("status");
+    	this.status = (int)jN.doubleValue();
     	jN = (JSONNumber)jO.get("turns");
     	this.turns = (int)jN.doubleValue();
     	jN = (JSONNumber)jO.get("X");
@@ -51,9 +52,17 @@ public class Module {
     public void setCode(int code) {
     	this.code=code;
     }
-    public void setStatus(String status) {
-    	this.status=status;
-    }
+    public String getStringStatus()
+	{
+		if(this.status == 0)
+			theString = "Undamaged";
+		if(this.status == 1)
+			theString = "Repairable";
+		if(this.status == 2)
+			theString = "Unrepairable";
+		else theString = "";
+		return theString;
+	}
     public void setTurns(int turns) {
     	this.turns=turns;
     }
@@ -69,7 +78,7 @@ public class Module {
     public String toString(){
     	StringBuilder aStringBuilder = new StringBuilder();
     	aStringBuilder.append("code:").append(Integer.toString(this.code)).append(", status\"")
-    	.append(this.status).append("\", turns:").append(Integer.toString(this.turns)).append(", X:")
+    	.append(this.theString).append("\", turns:").append(Integer.toString(this.turns)).append(", X:")
     	.append(Integer.toString(this.xcoord)).append(", Y:").append(Integer.toString(this.ycoord));
 		return aStringBuilder.toString();
     }
