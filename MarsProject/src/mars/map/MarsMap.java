63d731792e41db82306d230af0e3f705f7f155f8
Ezekial666 <manofsteel1665@gmail.com>
2015-04-26 22:24:41 -0500

    Update Map with min confige 1 and show current config complete //TODO load actual modules

--- MarsProject/src/mars/map/MarsMap.java
+++ MarsProject/src/mars/map/MarsMap.java
@@ -21,141 +21,382 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class MarsMap {
 	static final String unsupportedBrowser = "Your browser does not support the HTML5 Canvas";
-	static final int height = 600;
-	static final int width = 800;
+	static final int HEIGHT = 600;
+	static final int WIDTH = 800;
 	Canvas canvas;
 	private Storage localStorage;
 	private ArrayList<Module> list;
-	
+	private boolean displayFullConfig1 = false;
+	private boolean displayFullConfig2 = false;
+	private boolean displayMinConfig1 = false;
+	private boolean displayMinConfig2 = false;
+	private boolean displayCurrentConfig = true;
 	VerticalPanel panel = new VerticalPanel();
 	Context2d context;
-	public MarsMap(Storage localStorage){
+
+	public MarsMap(Storage localStorage) {
 		this.localStorage = localStorage;
 		canvas = Canvas.createIfSupported();
 		if (canvas == null) {
-			//RootPanel.get(divTagId).add(new Label(unsupportedBrowser));
+			// RootPanel.get(divTagId).add(new Label(unsupportedBrowser));
 			return;
 		}
 		createCanvas();
 	}
-	public ArrayList<Module> getSavedModules()
-	 {
-		 if (localStorage != null) { 
-			 list.clear();
-		//NOTE: when we iterate through this, we can possibly add the modules to the "currentmodulelist" or whatever
-			 for (int i = 0; i < localStorage.getLength(); i += 1) { 
-				 String key = localStorage.key(i); 
-				 Window.alert(key);
-				 String value = localStorage.getItem(key); 
-				 Window.alert(value);
-				 String[] values = value.split(" ");
-				 list.add(new Module(Integer.parseInt(values[0]), Integer.parseInt(values[1]), Integer.parseInt(values[2]), Integer.parseInt(values[3]), Integer.parseInt(values[4])));
-			 }
-		 } else{
-			 Window.alert("MODULE STORAGE IS NULL");
-		 }
-		 return list;
-	 }
-	private void createCanvas(){
 
-		canvas.setWidth(width + "px");
-		canvas.setHeight(height + "px");
-		canvas.setCoordinateSpaceWidth(width);
-		canvas.setCoordinateSpaceHeight(height);
-		//RootPanel.get(divTagId).add(canvas); for raw use of canvas
+	public ArrayList<Module> getSavedModules() {
+		if (localStorage != null) {
+			list.clear();
+			// NOTE: when we iterate through this, we can possibly add the
+			// modules to the "currentmodulelist" or whatever
+			for (int i = 0; i < localStorage.getLength(); i += 1) {
+				String key = localStorage.key(i);
+				Window.alert(key);
+				String value = localStorage.getItem(key);
+				Window.alert(value);
+				String[] values = value.split(" ");
+				list.add(new Module(Integer.parseInt(values[0]), Integer
+						.parseInt(values[1]), Integer.parseInt(values[2]),
+						Integer.parseInt(values[3]), Integer
+								.parseInt(values[4])));
+			}
+		} else {
+			Window.alert("MODULE STORAGE IS NULL");
+		}
+		return list;
+	}
+
+	private void createCanvas() {
+
+		canvas.setWidth(WIDTH + "px");
+		canvas.setHeight(HEIGHT + "px");
+		canvas.setCoordinateSpaceWidth(WIDTH);
+		canvas.setCoordinateSpaceHeight(HEIGHT);
+		// RootPanel.get(divTagId).add(canvas); for raw use of canvas
 
 		context = canvas.getContext2d(); // a rendering context
-
-       
 
 		final Image img = new Image("images/crater.jpg");
 		img.setVisible(true);
 		final ImageElement crater = ImageElement.as(img.getElement());
 		img.addLoadHandler(new LoadHandler() {
-			public void onLoad(LoadEvent event) { // fired by RootPanel.get().add
-				context.drawImage(crater,0,0);
+			public void onLoad(LoadEvent event) { // fired by
+													// RootPanel.get().add
+				context.drawImage(crater, 0, 0);
 			}
-		}); 
-	    canvas.setVisible(true);
-		//panel.add(canvas);
-		
-	//	RootPanel.get().add(canvas);
-	    img.setVisible(false);     // two line hack to ensure image is loaded
-		RootPanel.get().add(img);  // image must be on page to fire load event 
-	
+		});
+		canvas.setVisible(true);
+		// panel.add(canvas);
+
+		// RootPanel.get().add(canvas);
+		img.setVisible(false); // two line hack to ensure image is loaded
+		RootPanel.get().add(img); // image must be on page to fire load event
+
 	}
-	public Canvas getCanvas(){
+
+	public Canvas getCanvas() {
 		return canvas;
 	}
-	public void addModuleToMap(Module moduleType){
+
+	public void addModuleToMap(Module moduleType) {
 		Module adModule = moduleType;
 		final Image moduleImage;
-	   if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Canteen.jpg");
-	   } else if(moduleType.getCode() > 20){
-		   moduleImage = new Image("images/Sanitation.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Airlock.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Control.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Food.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Gym.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Plain.jpg");
-	   } else if(moduleType.getCode() < 20){
-		   moduleImage = new Image("images/Power.jpg");
-		   
-	   } else{
-		   moduleImage = new Image("images/Power.jpg");
-	   }
-	   moduleImage.setVisible(true);
+		if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Canteen.jpg");
+		} else if (moduleType.getCode() > 20) {
+			moduleImage = new Image("images/Sanitation.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Airlock.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Control.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Food.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Gym.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Plain.jpg");
+		} else if (moduleType.getCode() < 20) {
+			moduleImage = new Image("images/Power.jpg");
+
+		} else {
+			moduleImage = new Image("images/Power.jpg");
+		}
+		moduleImage.setVisible(true);
 		final ImageElement module = ImageElement.as(moduleImage.getElement());
 		final int x = adModule.getX();
 		moduleImage.addLoadHandler(new LoadHandler() {
 			public void onLoad(LoadEvent event) {
-				//final int x= xPos;
-				//final int y = yPos; // fired by RootPanel.get().add
-				context.drawImage(module,x,x);
+				// final int x= xPos;
+				// final int y = yPos; // fired by RootPanel.get().add
+				context.drawImage(module, x, x);
 			}
-		}); 
-	   moduleImage.setVisible(false);     // two line hack to ensure image is loaded
+		});
+		moduleImage.setVisible(false); // two line hack to ensure image is
+										// loaded
 		RootPanel.get().add(moduleImage);
 	}
-     
-	public DockPanel getMarsPanel(){
-		DockPanel marsPanel  = new DockPanel();
+
+	public DockPanel getMarsPanel() {
+		DockPanel marsPanel = new DockPanel();
 		marsPanel.add(getCanvas(), DockPanel.NORTH);
-		 final Button displayConfig1 = new Button("Minimun Config 1");
-	        displayConfig1.addClickHandler(new ClickHandler(){
-	        	public void onClick(ClickEvent event) {
-	        		//TODO
-	        	}
-	        });
-	        final Button displayConfig2 = new Button("Minimun Config 2");
-	        displayConfig2.addClickHandler(new ClickHandler(){
-	        	public void onClick(ClickEvent event) {
-	        		//TODO
-	        	}
-	        });
-	        final Button displayConfig3 = new Button("Display Config 3");
-	        displayConfig3.addClickHandler(new ClickHandler(){
-	        	public void onClick(ClickEvent event) {
-	        		//TODO
-	        	}
-	        });
-	        final Button displayConfig4 = new Button("Display Config 4");
-	        displayConfig4.addClickHandler(new ClickHandler(){
-	        	public void onClick(ClickEvent event) {
-	        		//TODO
-	        	}
-	        });
+		final Button displayConfig1 = new Button("Display Full Configuration 1");
+		displayConfig1.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				displayFullConfig1 = true;
+				displayFullConfig2 = false;
+				displayMinConfig1 = false;
+				displayMinConfig2 = false;
+				displayCurrentConfig = false;
+				updateMap();
+			}
+		});
+		final Button displayConfig2 = new Button("Display Full Configuration 2");
+		displayConfig2.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				displayFullConfig1 = false;
+				displayFullConfig2 = true;
+				displayMinConfig1 = false;
+				displayMinConfig2 = false;
+				displayCurrentConfig = false;
+				updateMap();
+			}
+		});
+		final Button displayConfig3 = new Button(
+				"Display Minimun Configuration 1");
+		displayConfig3.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				displayFullConfig1 = false;
+				displayFullConfig2 = false;
+				displayMinConfig1 = true;
+				displayMinConfig2 = false;
+				displayCurrentConfig = false;
+				updateMap();
+			}
+		});
+		final Button displayConfig4 = new Button(
+				"Display Minimun Configuration 2");
+		displayConfig4.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				displayFullConfig1 = false;
+				displayFullConfig2 = false;
+				displayMinConfig1 = false;
+				displayMinConfig2 = true;
+				displayCurrentConfig = false;
+				updateMap();
+			}
+		});
+		final Button displayCurrent = new Button(
+				"Display Current Configuration");
+		displayCurrent.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				displayFullConfig1 = false;
+				displayFullConfig2 = false;
+				displayMinConfig1 = false;
+				displayMinConfig2 = false;
+				displayCurrentConfig = true;
+				updateMap();
+			}
+		});
 		marsPanel.add(displayConfig4, DockPanel.WEST);
 		marsPanel.add(displayConfig3, DockPanel.WEST);
 		marsPanel.add(displayConfig1, DockPanel.EAST);
 		marsPanel.add(displayConfig2, DockPanel.EAST);
+		marsPanel.add(displayCurrent, DockPanel.SOUTH);
+
 		return marsPanel;
 	}
-	
+
+	void updateMap() {
+		if (displayFullConfig1 == true) {
+
+		} else if (displayFullConfig2 == true) {
+
+		} else if (displayMinConfig1 == true) {
+			context.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+
+			final Image img = new Image("images/crater.jpg");
+			img.setVisible(true);
+			final ImageElement crater = ImageElement.as(img.getElement());
+			img.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(crater, 0, 0);
+				}
+			});
+			canvas.setVisible(true);
+			img.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img); // image must be on page to fire load
+										// event
+			final Image img1 = new Image("images/GreyPlain.jpg");
+			img1.setVisible(true);
+			final ImageElement greyPlain1 = ImageElement.as(img1.getElement());
+			img1.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(greyPlain1, 300, 300);
+				}
+			});
+			canvas.setVisible(true);
+			img1.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img1);
+			final Image img2 = new Image("images/GreyPlain.jpg");
+			img2.setVisible(true);
+			final ImageElement greyPlain2 = ImageElement.as(img2.getElement());
+			img2.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(greyPlain2, 300, 350);
+				}
+			});
+			canvas.setVisible(true);
+			img2.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img2);
+			final Image img3 = new Image("images/GreyPlain.jpg");
+			img3.setVisible(true);
+			
+			final ImageElement greyPlain3 = ImageElement.as(img3.getElement());
+			img3.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(greyPlain3, 300, 400);
+				}
+			});
+			canvas.setVisible(true);
+			img3.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img3);
+
+			final Image img4 = new Image("images/Greycontrol.jpg");
+			img4.setVisible(true);
+			
+			final ImageElement control = ImageElement.as(img4.getElement());
+			img4.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(control, 250, 350);
+				}
+			});
+			canvas.setVisible(true);
+			img4.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img4);
+
+			
+			final Image img5 = new Image("images/GreyGym.jpg");
+			img5.setVisible(true);
+			final ImageElement sanitation = ImageElement.as(img5.getElement());
+			img5.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(sanitation, 250, 400);
+				}
+			});
+			canvas.setVisible(true);
+			img5.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img5);
+
+			
+			
+			
+			
+			
+			//Airlock
+			final Image img6 = new Image("images/GreyAirLock.jpg");
+			img6.setVisible(true);
+			final ImageElement airlockImage = ImageElement.as(img6.getElement());
+			img6.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(airlockImage, 300, 450);
+				}
+			});
+			canvas.setVisible(true);
+			img6.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img6);
+
+			
+			//Dorm
+			final Image img7 = new Image("images/GreyDormitory.jpg");
+			img7.setVisible(true);
+			final ImageElement dormImage = ImageElement.as(img7.getElement());
+			img7.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(dormImage, 350, 300);
+				}
+			});
+			canvas.setVisible(true);
+			img7.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img7);
+
+			//power
+			final Image img8 = new Image("images/GreyPower.jpg");
+			img8.setVisible(true);
+			final ImageElement powerImage = ImageElement.as(img8.getElement());
+			img8.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(powerImage, 350, 350);
+				}
+			});
+			canvas.setVisible(true);
+			img8.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img8);
+
+			//food
+			final Image img9 = new Image("images/GreyCanteen.jpg");
+			img9.setVisible(true);
+			final ImageElement foodImage = ImageElement.as(img9.getElement());
+			img9.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(foodImage, 300, 250);
+				}
+			});
+			canvas.setVisible(true);
+			img9.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img9);
+
+			//storage
+			final Image img10 = new Image("images/GreyFood.jpg");
+			img10.setVisible(true);
+			final ImageElement storageImage = ImageElement.as(img10.getElement());
+			img10.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(storageImage, 250, 300);
+				}
+			});
+			canvas.setVisible(true);
+			img10.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img10);
+
+			
+			
+		} else if (displayMinConfig2 == true) {
+			context.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+		} else if (displayCurrentConfig == true) {
+			context.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+
+			final Image img = new Image("images/crater.jpg");
+			img.setVisible(true);
+			final ImageElement crater = ImageElement.as(img.getElement());
+			img.addLoadHandler(new LoadHandler() {
+				public void onLoad(LoadEvent event) { // fired by
+														// RootPanel.get().add
+					context.drawImage(crater, 0, 0);
+				}
+			});
+			canvas.setVisible(true);
+			img.setVisible(false); // two line hack to ensure image is loaded
+			RootPanel.get().add(img); // image must be on page to fire load
+										// event
+
+		}
+		loadModuleImages();
+
+	}
+
+	public void loadModuleImages() {
+
+	}
+
 }
