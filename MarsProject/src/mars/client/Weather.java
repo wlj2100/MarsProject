065c413c6f5c0c9a69a7687b8d9160afe55f29a4
frees033 <frees033@d.umn.edu>
2015-05-05 17:38:58 -0500

    Weather with sunset

--- MarsProject/src/mars/client/Weather.java
+++ MarsProject/src/mars/client/Weather.java
@@ -44,10 +44,12 @@ public class Weather{
      */
     public Weather(){
         
-        String url= URL.encode("http://api.wunderground.com/api/a22fd2e281653ddf/conditions/q/55805.json?");
-        String url2 = URL.encode("http://api.wunderground.com/api/a22fd2e281653ddf/astronomy/q/55805.json?");
+        String url= URL.encode("http://api.wunderground.com/api/33fb55060d9d7179/conditions/q/99791.json?");
+        String url2 = URL.encode("http://api.wunderground.com/api/33fb55060d9d7179/astronomy/q/55805.json?");
         
-        //Next section is performing JSON request to wunderground servers and calling update methods.
+        
+        
+        //Next two sections are performing JSON request to wunderground servers and calling update methods.
         this.jsonp = new JsonpRequestBuilder();
         this.jsonp.setCallbackParam("callback");
         this.jsonp.requestObject(url,  new AsyncCallback<JavaScriptObject>() {
@@ -61,9 +63,9 @@ public class Weather{
                 update( obj.toString() );              
             }     
         });
-        
+    
         this.jsonp1 = new JsonpRequestBuilder();
-        this.jsonp1.setCallbackParam("Callback");        
+        this.jsonp1.setCallbackParam("callback");        
         this.jsonp1.requestObject(url2,  new AsyncCallback<JavaScriptObject>() {
             public void onFailure(final Throwable caught){
                 Window.alert("Json onFailure");
@@ -75,10 +77,9 @@ public class Weather{
                 update2( obj.toString() );              
             }     
         });
-    
         
         this.weatherTable = new FlowPanel();
-        this.weatherTable.setStyleName("Weather");
+        this.weatherTable.setStyleName("weatherPanel");
         this.visString = new String("Visibility: ");
         this.tempString = new String("Temperature:  ");
         
@@ -99,7 +100,6 @@ public class Weather{
     public FlowPanel getWeather() {
         return this.weatherTable;
     }
-
     
     /**
      * Parses a JSONObject for relevant values then 
@@ -115,26 +115,28 @@ public class Weather{
         this.stringTemp = conditions.get("temp_c").toString();
         this.stringVis = conditions.get("visibility_km").toString();
         
+        
+        
+
         this.weatherTable.add(this.weatherHeader);
         this.weatherTable.add(new Label(this.tempString + "  " + this.stringTemp + " Degrees C"));
         this.weatherTable.add(new Label(this.visString + this.stringVis + " Km"));
         if(stringHour.equals("undefined")) {
-            stringHour = "13";
-            stringMinute = "12";
+            stringHour = "12";
+            stringMinute = "08";
         }
         this.weatherTable.add(new Label("Sunset time: " + this.stringHour + ":" + this.stringMinute));
-        
         this.weatherTable.add(this.wunderLogo);
         
   
     }
     
+    
     /**
-     * Parses a JSONObject the values for the sunset and 
-     * adds these values to the weatherTable.
+     * Parses a JSONObject for relevant values then 
+     * adds the values to the weatherTable.
      * @param conditions_ - The current weather conditions
      */
-    
     public void update2(final String astronomy_) {
         JSONObject astronomy = (JSONObject) JSONParser.parseLenient(astronomy_);
         String current = astronomy.get("sun_phase").toString();
@@ -147,6 +149,5 @@ public class Weather{
         this.stringMinute = astronomy.get("minute").toString();
 
     }
-    
     
 }
