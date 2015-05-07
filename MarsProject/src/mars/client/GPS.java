945502c9b71963ae1154a21ae45f1c84c09541f7
wlj2100 <wlj2100@gmail.com>
2015-05-07 13:21:01 -0500

    gps

--- MarsProject/src/mars/client/GPS.java
+++ MarsProject/src/mars/client/GPS.java
@@ -40,7 +40,7 @@ public class GPS {
 	private final FlexTable t = new FlexTable();
 
 	// php script
-	private String proxy = "http://www.d.umn.edu/~wang4881/war/proxy.php?q=";
+	private String proxy = "http://www.d.umn.edu/~abrooks/proxy.php?q=";
 
 	/**
 	 * Default CTOR
